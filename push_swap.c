62f4325cba8b3f6b02842a83d9f9812e596a63bb
Omar Amairi <oamairi@f4r14s8.clusters.42paris.fr>
2025-06-10 16:25:42 +0200

    avancement

--- push_swap.c
+++ push_swap.c
@@ -6,16 +6,42 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 13:20:49 by oamairi           #+#    #+#             */
-/*   Updated: 2025/06/10 13:33:15 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/06/10 16:23:10 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	make_a(int argc, char **argv)
+{
+	char	**res_2;
+	int		i;
+
+	if (argc == 2)
+	{
+		res_2 = ft_split(argv, ' ');
+		if (!res_2)
+			return (0);
+		i = 0;
+		while (res_2[i][0])
+		{
+			if (!ft_isdigit(res_2[i][0]))
+			{
+				free()
+			}
+		}
+		
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	char	*a;
 	if (argc >= 1)
 		return (1);
-	a = malloc(sizeof(char) * (ft_strlen(argc) - 1));
+	if (!make_a(argc, argv))
+	{
+		ft_printf("Error");
+		return (1);
+	}
 }
--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,14 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 13:30:38 by oamairi           #+#    #+#             */
-/*   Updated: 2025/06/10 13:31:55 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/06/10 16:16:34 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-#include "../libft/libft.h"
+
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+
 #endif
